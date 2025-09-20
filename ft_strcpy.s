05a43e582a65289442b9daadd3909b794129800d
Adrian Pena barragan <apena-ba@c3r9s4.42malaga.com>
2025-09-20 18:40:24 +0200

    Fix tabs

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -2,13 +2,13 @@ section .text
     global ft_strcpy
 
 ft_strcpy:
-    mov rax, rdi            ; Keep dest to return
-    mov rcx, -1             ; Initialize counter to -1
+    mov rax, rdi         ; Keep dest to return
+    mov rcx, -1          ; Initialize counter to -1
 
 _loop:
     inc rcx
-    mov dl, [rsi + rcx]     ; Use the register dl to store the value
+    mov dl, [rsi + rcx]  ; Use the register dl to store the value
     mov [rdi + rcx], dl
-    cmp dl, 0               ; If the char is not \0, keep looping
+    cmp dl, 0            ; If the char is not \0, keep looping
     jne _loop
     ret
